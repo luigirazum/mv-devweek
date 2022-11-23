71ac832363dc33e389a926c76323c39b040f9071
Luis Zubia <soyzubia@gmail.com>
2022-11-22 18:54:02 -0600

    Add array variable speakers to js file to store the speakers info.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,141 @@
 // This file contains the JS scripts/functions used in the DevWeek project.
+
+// Object to store Speakers info
+
+const speakers = [
+  {
+    id: 'speaker1',
+    name: 'Phil Wilkins',
+    position: 'Cloud Developer Evangelist @ Oracle',
+    bio: `Phil Wilkins has spent over 25 years in the
+          software industry with a breadth of experience
+          in different types of businesses and environments
+          from multinationals to software startups and
+          consumer organizations including a global optical
+          and auditory healthcare provider. He started out
+          as a developer on real-time mission-critical
+          solutions and has worked his way up through
+          technical and development leadership roles
+          primarily in Java-based environments. These days
+          Phil focuses on APIs, Integration, and development
+          technologies and techniques. In 2022 Phil joined
+          Oracle as a Cloud Developer Evangelist building on
+          his previous work contributing to the developer
+          community.<br>
+          Phil has authored or co-authored books on Fluentd,
+          API, and Integration tools. He has also had a number
+          of articles published in technical journals in his
+          own right as well as being an active blogger. When
+          not writing Phil is exploring new tech or presenting
+          at conferences physically & virtually around the
+          world - from DeveloperWeek to JAXLondon.`,
+    photo: './assets/imgs/phil-wilkins.jpeg'
+  },
+  {
+    id: 'speaker2',
+    name: 'Jody Bailey',
+    position: 'Chief Technology Officer @ Stack Overflow',
+    bio: `Jody Bailey is the Chief Technology Officer at
+          Stack Overflow, leading the Product Engineering,
+          Platform Engineering, InfoSec, and IT teams. Jody
+          has spent the last eight years of his nearly
+          30-year career on tech-leading EdTech software
+          development teams. Most recently, Jody served as
+          a senior product development leader at AWS, where
+          he led the Product Management, User Experience,
+          and Engineering teams responsible for new
+          self-paced learning experiences for AWS customers.
+          Prior to AWS, he was the Chief Technology Officer
+          at Pluralsight where he grew the development team
+          by 10X and transformed the product into an
+          industry-leading enterprise SaaS learning offering.
+          Jody is married and has three children. Outside
+          of work Jody enjoys spending time with his family,
+          traveling, mountain bike racing, sailing and
+          listening to live music.`,
+    photo: './assets/imgs/jody-bailey.png'
+  },
+  {
+    id: 'speaker3',
+    name: 'Spencer Carver',
+    position: 'Software Engineer @ Bloomberg Media',
+    bio: `Spencer Carver has been passionate about web
+          development since joining Bloomberg in 2015.
+          He is responsible for various aspects of
+          bloomberg.com’s subscription products, and is
+          consistently working to improve the first-touch
+          experience of new visitors on the news site
+          through experimentation and clearer on-site
+          experiences. Outside of the technical space,
+          he can occasionally be found rock climbing,
+          playing board games, or continuing his quest to
+          visit as many U.S. National Parks as possible.
+          As a home chef, his dishes frequently receive
+          spousal acclaim for being “too spicy” or “good
+          enough”.`,
+    photo: './assets/imgs/spencer-carver.jpg'
+  }//,
+  // {
+  //   id: '',
+  //   name: '',
+  //   position: '',
+  //   bio: ``,
+  //   photo: ''
+  // },
+  // {
+  //   id: '',
+  //   name: '',
+  //   position: '',
+  //   bio: ``,
+  //   photo: ''
+  // },
+  // {
+  //   id: '',
+  //   name: '',
+  //   position: '',
+  //   bio: ``,
+  //   photo: ''
+  // },
+  // {
+  //   id: '',
+  //   name: '',
+  //   position: '',
+  //   bio: ``,
+  //   photo: ''
+  // },
+  // {
+  //   id: '',
+  //   name: '',
+  //   position: '',
+  //   bio: ``,
+  //   photo: ''
+  // },
+  // {
+  //   id: '',
+  //   name: '',
+  //   position: '',
+  //   bio: ``,
+  //   photo: ''
+  // },
+  // {
+  //   id: '',
+  //   name: '',
+  //   position: '',
+  //   bio: ``,
+  //   photo: ''
+  // },
+  // {
+  //   id: '',
+  //   name: '',
+  //   position: '',
+  //   bio: ``,
+  //   photo: ''
+  // },
+  // {
+  //   id: '',
+  //   name: '',
+  //   position: '',
+  //   bio: ``,
+  //   photo: ''
+  // }
+];
